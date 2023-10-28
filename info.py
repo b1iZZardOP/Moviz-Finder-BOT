947787e224633a43c5a61ae72de312080d4a9ccc
b1iZZardOP <85004244+b1iZZardOP@users.noreply.github.com>
2023-10-28 11:46:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ PORT = environ.get("PORT", "8080")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/cad7038fe82e47f79c609.jpg')).split()
+PICS = (environ.get('PICS', 'https://ibb.co/LPKrqGd')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '744133991').split()]
