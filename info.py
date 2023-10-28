93cec9856cd4e4e79bdcfb562be833c2a4ae981d
b1iZZardOP <85004244+b1iZZardOP@users.noreply.github.com>
2023-10-28 13:05:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1797673917')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1797673917'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'LazyPrincessSupport')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), False)
